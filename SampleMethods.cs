9770519246f332a9c82a7ec47abe4415d7fe7228
Vijaya Kumar Pulugu <puluguv@hotmail.com>
2023-02-24 23:20:37 -0600

    Add project files.

---
+++ SampleMethods.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace PowerApps.Samples
+{
+    public partial class SampleProgram
+    {
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="svc"></param>
+        /// <returns></returns>
+        private static EntityCollection GetEntitiesToUpdate(CrmServiceClient svc)
+        {
+            QueryExpression queryExpression = new QueryExpression("contact");
+            queryExpression.ColumnSet = new ColumnSet("fullname", "firstname", "lastname");
+            return svc.RetrieveMultiple(queryExpression);
+        }
+
+        /// <summary>
+        /// Creates entities in parallel
+        /// </summary>
+        /// <param name="svc">The CrmServiceClient instance to use</param>
+        /// <param name="entities">A List of entities to create.</param>
+        /// <returns></returns>
+        private static ConcurrentBag<EntityReference> UpdateEntities(CrmServiceClient svc, DataCollection<Entity> entities)
+        {
+            var updatedEntityReferences = new ConcurrentBag<EntityReference>();
+
+            Parallel.ForEach(entities, new ParallelOptions() { MaxDegreeOfParallelism = 10 }, 
+                () =>  
+                {
+                //Clone the CrmServiceClient for each thread
+                    return svc.Clone();
+                },
+                (entity, loopState, index, threadLocalSvc) =>
+                {
+                    //Console.WriteLine(loopState);
+                    Console.WriteLine(index);
+                    // In each thread, update entities and add them to the ConcurrentBag
+                    // as EntityReferences
+
+                    string existingFullName = (string)entity.Attributes["fullname"];
+                    string newFullName = null;
+                    string[] names = existingFullName.Split(' ');
+                    
+                    
+                    if(names.Length>1)
+                    {
+                        newFullName = names[1] + " " + names[0];//Build logic to handle  more than 2 values in name split
+                        entity.Attributes["fullname"] = newFullName;
+                    }
+                    threadLocalSvc.Update(entity);
+                    updatedEntityReferences.Add(
+                        new EntityReference(
+                            entity.LogicalName,
+                            entity.Id)
+                        );
+
+                    return threadLocalSvc;
+                },
+                (threadLocalSvc) =>
+                {
+                    //Dispose the cloned CrmServiceClient instance
+                    if (threadLocalSvc != null)
+                    {
+                        threadLocalSvc.Dispose();
+                    }
+                });
+
+            //Return the ConcurrentBag of EntityReferences
+            return updatedEntityReferences;
+        }
+
+        /// <summary>
+        /// Gets web service connection information from the app.config file.
+        /// If there is more than one available, the user is prompted to select
+        /// the desired connection configuration by name.
+        /// </summary>
+        /// <returns>A string containing web service connection configuration information.</returns>
+        private static string GetServiceConfiguration()
+        {
+            // Get available connection strings from app.config.
+            int count = ConfigurationManager.ConnectionStrings.Count;
+
+            // Create a filter list of connection strings so that we have a list of valid
+            // connection strings for Common Data Service only.
+            List<KeyValuePair<String, String>> filteredConnectionStrings =
+                new List<KeyValuePair<String, String>>();
+
+            for (int a = 0; a < count; a++)
+            {
+                if (isValidConnectionString(ConfigurationManager.ConnectionStrings[a].ConnectionString))
+                    filteredConnectionStrings.Add
+                        (new KeyValuePair<string, string>
+                            (ConfigurationManager.ConnectionStrings[a].Name,
+                            ConfigurationManager.ConnectionStrings[a].ConnectionString));
+            }
+
+            // No valid connections strings found. Write out and error message.
+            if (filteredConnectionStrings.Count == 0)
+            {
+                Console.WriteLine("An app.config file containing at least one valid Common Data Service " +
+                    "connection string configuration must exist in the run-time folder.");
+                Console.WriteLine("\nThere are several commented out example connection strings in " +
+                    "the provided app.config file. Uncomment one of them and modify the string according " +
+                    "to your Common Data Service installation. Then re-run the sample.");
+                return null;
+            }
+
+            // If one valid connection string is found, use that.
+            if (filteredConnectionStrings.Count == 1)
+            {
+                return filteredConnectionStrings[0].Value;
+            }
+
+            // If more than one valid connection string is found, let the user decide which to use.
+            if (filteredConnectionStrings.Count > 1)
+            {
+                Console.WriteLine("The following connections are available:");
+                Console.WriteLine("------------------------------------------------");
+
+                for (int i = 0; i < filteredConnectionStrings.Count; i++)
+                {
+                    Console.Write("\n({0}) {1}\t",
+                    i + 1, filteredConnectionStrings[i].Key);
+                }
+
+                Console.WriteLine();
+
+                Console.Write("\nType the number of the connection to use (1-{0}) [{0}] : ",
+                    filteredConnectionStrings.Count);
+                String input = Console.ReadLine();
+                int configNumber;
+                if (input == String.Empty) input = filteredConnectionStrings.Count.ToString();
+                if (!Int32.TryParse(input, out configNumber) || configNumber > count ||
+                    configNumber == 0)
+                {
+                    Console.WriteLine("Option not valid.");
+                    return null;
+                }
+
+                return filteredConnectionStrings[configNumber - 1].Value;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Verifies if a connection string is valid for Common Data Service.
+        /// </summary>
+        /// <returns>True for a valid string, otherwise False.</returns>
+        private static bool isValidConnectionString(String connectionString)
+        {
+            // At a minimum, a connection string must contain one of these arguments.
+            if (connectionString.Contains("Url=") ||
+                connectionString.Contains("Server=") ||
+                connectionString.Contains("ServiceUri="))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SampleProgram.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PowerApps.Samples
+{
+    public partial class SampleProgram
+    {
+
+        //How many records to create with this sample.
+        private static readonly int numberOfRecords = 10000;
+
+        [STAThread] // Added to support UX
+        private static void Main()
+        {
+
+            #region Optimize Connection settings
+
+            //Change max connections from .NET to a remote service default: 2
+            System.Net.ServicePointManager.DefaultConnectionLimit = 65000;
+            //Bump up the min threads reserved for this app to ramp connections faster - minWorkerThreads defaults to 4, minIOCP defaults to 4
+            System.Threading.ThreadPool.SetMinThreads(500, 500);
+            //Turn off the Expect 100 to continue message - 'true' will cause the caller to wait until it round-trip confirms a connection to the server
+            System.Net.ServicePointManager.Expect100Continue = false;
+            //Can decreas overall transmission overhead but can cause delay in data packet arrival
+            System.Net.ServicePointManager.UseNagleAlgorithm = false;
+
+            #endregion Optimize Connection settings
+
+            CrmServiceClient service = null;
+
+            try
+            {
+
+                var clientId = "<<CLIENT-ID>>";
+                var secret = "<<CLIENT-SECRET>>";
+                var dataVerseUrl = "<<ORG-URL>>";
+
+                var connectionString = $"AuthType=ClientSecret;" +
+                                       $"url={dataVerseUrl};" +
+                                       $"ClientId={clientId};" +
+                                       $"ClientSecret={secret};" +
+                                       $"RequireNewInstance=false;";
+
+                service = new CrmServiceClient(connectionString);
+
+
+                if (service.IsReady)
+                {
+                    #region Code
+
+                    ////////////////////////////////////
+
+                    #region Demonstrate
+
+
+                    try
+                    {
+
+                        var startCreate = DateTime.Now;
+
+                        //Retrieve records to update 
+                        var entitiesToUpdate = GetEntitiesToUpdate(service);
+
+
+                        //Import the list of entities
+                        var updatedEntities = UpdateEntities(service, entitiesToUpdate.Entities);
+
+                        //capture time for execution
+                        var secondsToCreate = (DateTime.Now - startCreate).TotalSeconds;
+
+                        Console.WriteLine($"updated {updatedEntities.Count} entities in  {Math.Round(secondsToCreate)} seconds.");
+
+
+                    }
+                    catch (AggregateException)
+                    {
+                        // Handle exceptions
+                    }
+
+                    Console.WriteLine("Done.");
+                    Console.ReadLine();
+                }
+
+
+                #endregion Demonstrate
+
+                #endregion Code
+
+                else
+                {
+                    const string UNABLE_TO_LOGIN_ERROR = "Unable to Login to Microsoft Dataverse";
+                    if (service.LastCrmError.Equals(UNABLE_TO_LOGIN_ERROR))
+                    {
+                        Console.WriteLine("Check the connection string values in cds/App.config.");
+                        throw new Exception(service.LastCrmError);
+                    }
+                    else
+                    {
+                        throw service.LastCrmException;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (service != null)
+                    service.Dispose();
+
+                Console.WriteLine("Press <Enter> to exit.");
+                Console.ReadLine();
+            }
+        }
+
+
+
+    }
+}
